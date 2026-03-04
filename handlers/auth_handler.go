32f67b6d183648f88f63478d908391eb5e5f08de
Wizzyszn <wisdomolubayoadedoyin@yahoo.com>
2026-03-04 18:00:14 +0100

    fix: rename NewAuthService to NewAuthHandler for naming consistency

--- handlers/auth_handler.go
+++ handlers/auth_handler.go
@@ -13,7 +13,7 @@ type AuthHandler struct {
 	authService *service.AuthService
 }
 
-func NewAuthService(authService *service.AuthService) *AuthHandler {
+func NewAuthHandler(authService *service.AuthService) *AuthHandler {
 	return &AuthHandler{
 		authService: authService,
 	}
